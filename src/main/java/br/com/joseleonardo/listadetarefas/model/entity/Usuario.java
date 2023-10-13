f0e26334bd0da0a92a6d23c1fcffb878a4fb77f6
José Leonardo <leonardo_cordeirobahia@hotmail.com>
2023-10-13 11:18:17 -0300

    Adicionando o Lombok no projeto

--- src/main/java/br/com/joseleonardo/listadetarefas/model/entity/Usuario.java
+++ src/main/java/br/com/joseleonardo/listadetarefas/model/entity/Usuario.java
@@ -1,33 +1,12 @@
 package br.com.joseleonardo.listadetarefas.model.entity;
 
+import lombok.Data;
+
+@Data // Coloca os Getters e Setters para todos os atributos
 public class Usuario {
 
 	private String nome;
 	private String nomeDeUsuario;
 	private String senha;
-
-	public String getNome() {
-		return nome;
-	}
-
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-
-	public String getNomeDeUsuario() {
-		return nomeDeUsuario;
-	}
-
-	public void setNomeDeUsuario(String nomeDeUsuario) {
-		this.nomeDeUsuario = nomeDeUsuario;
-	}
-
-	public String getSenha() {
-		return senha;
-	}
-
-	public void setSenha(String senha) {
-		this.senha = senha;
-	}
 
 }
