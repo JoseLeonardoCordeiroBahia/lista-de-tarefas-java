9d00c5708d97371c099be568afa5189a1d0a4dd2
José Leonardo <leonardo_cordeirobahia@hotmail.com>
2023-10-13 11:21:23 -0300

    Criando a tabela do usuário no banco de dados utilizando o JPA

--- src/main/java/br/com/joseleonardo/listadetarefas/model/entity/Usuario.java
+++ src/main/java/br/com/joseleonardo/listadetarefas/model/entity/Usuario.java
@@ -1,12 +1,28 @@
 package br.com.joseleonardo.listadetarefas.model.entity;
 
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import lombok.Data;
 
 @Data // Coloca os Getters e Setters para todos os atributos
+@Entity(name="usuario") // Nomeia a tabela no banco de dados
 public class Usuario {
 
+	@Id
+	@GeneratedValue(generator = "UUID") // O jakarta (JPA) gera o ID
+	private UUID id;
+	
 	private String nome;
 	private String nomeDeUsuario;
 	private String senha;
 
+	@CreationTimestamp
+	private LocalDateTime dataDeCriacao;
+	
 }
