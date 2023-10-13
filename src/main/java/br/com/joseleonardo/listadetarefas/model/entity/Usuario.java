c2e6e2a448343dc61bb42d7c49c8ccd81611c199
José Leonardo <leonardo_cordeirobahia@hotmail.com>
2023-10-13 11:17:04 -0300

    Criando a classe do usuário

---
+++ src/main/java/br/com/joseleonardo/listadetarefas/model/entity/Usuario.java
@@ -0,0 +1,33 @@
+package br.com.joseleonardo.listadetarefas.model.entity;
+
+public class Usuario {
+
+	private String nome;
+	private String nomeDeUsuario;
+	private String senha;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getNomeDeUsuario() {
+		return nomeDeUsuario;
+	}
+
+	public void setNomeDeUsuario(String nomeDeUsuario) {
+		this.nomeDeUsuario = nomeDeUsuario;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+}
