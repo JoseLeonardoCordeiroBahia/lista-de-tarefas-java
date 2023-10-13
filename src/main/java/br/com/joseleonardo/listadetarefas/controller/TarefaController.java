8412456ebeb984266f01c6174fee9c31bfbf3fc1
José Leonardo <leonardo_cordeirobahia@hotmail.com>
2023-10-13 11:34:24 -0300

    Criando validação para as datas de início e término da tarefa

--- src/main/java/br/com/joseleonardo/listadetarefas/controller/TarefaController.java
+++ src/main/java/br/com/joseleonardo/listadetarefas/controller/TarefaController.java
@@ -1,8 +1,11 @@
 package br.com.joseleonardo.listadetarefas.controller;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,14 +21,30 @@ public class TarefaController {
 
 	@Autowired
 	private TarefaRepository tarefaRepository;
-
+	
 	@PostMapping
-	public Tarefa criar(@RequestBody Tarefa tarefa, HttpServletRequest request) {
-		UUID idUsuario = (UUID) request.getAttribute("idDoUsuario");
-
-		tarefa.setIdDoUsuario(idUsuario);
+	public ResponseEntity<?> criar(@RequestBody Tarefa tarefa, HttpServletRequest request) {
+		Object idUsuario = request.getAttribute("idDoUsuario");
 		
-		return tarefaRepository.save(tarefa);
+		tarefa.setIdDoUsuario((UUID) idUsuario);
+		
+		LocalDateTime dataAtual = LocalDateTime.now();
+		
+		// 10/11/2023 - dataAtual
+		// 10/10/2023 - dataDeInicio
+		if (tarefa.getDataDeInicio().isBefore(dataAtual) || tarefa.getDataDeTermino().isBefore(dataAtual)) {
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+					.body("A data de início / data de término deve ser maior do que a data atual");
+		}
+		
+		if (tarefa.getDataDeInicio().isAfter(tarefa.getDataDeTermino())) {
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+					.body("A data de início deve ser menor do que a data de término");
+		}
+		
+		Tarefa tarefaCriada = tarefaRepository.save(tarefa);
+		
+		return ResponseEntity.status(HttpStatus.CREATED).body(tarefaCriada);
 	}
-
+	
 }
